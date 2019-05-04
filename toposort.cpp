d484075f22a8426fbe9eeb8ccc805c67a27e357f
Chitrang Srivastava <chitrang.srivastava@gmail.com>
2019-05-05 02:17:22 +0530

    Topological sort using Kahn Algorithm

--- toposort.cpp
+++ toposort.cpp
@@ -1,82 +1,63 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <algorithm>
-#include <vector>
-#include <stack>
 #define ONE_BASE // use this to define if input uses nodes start from 1, default 0-based nodes
 /*
  * Topological Sort: returns lex-smallest ordering
  * Input: DAG
 */
 
-using std::vector;
-using std::pair;
-using std::stack;
-bool sortBySize(vector<int> a, vector<int> b)
-{
-	return a.size() > b.size();
-}
+using namespace std;
 size_t n, m;
-vector<bool> visited;
-stack<int> nodes;
-vector< pair<int, int>> inDegree; // node index and its inDegree;
-void toposort(vector<vector<int> > &adj, int s) {
+vector<int> inDegree; // node index and its inDegree;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+void toposort(vector<vector<int> > &adj, int s) 
+{
 	for(int i=0; i<adj[s].size(); i++)
 	{
-		if(!visited[adj[s][i]])
-			toposort(adj, adj[s][i]);
+		{
+			inDegree[adj[s][i]]--;
+			if(!inDegree[adj[s][i]])
+				pq.push(adj[s][i]);
+		}
 	}
-	// Push the node on stack if either its leaf nodes or all its branch nodes are visited.
-	nodes.push(s); 
-	visited[s] = true; // Mark the node as visited
+	#ifdef ONE_BASE
+	cout <<s+1<<" ";
+	#else
+		cout <<s<<" ";
+	#endif
 }
-bool sortByDegree(pair<int , int> a, pair<int, int> b)
+int main() 
 {
-	if(a.second==b.second) // same in degree
-	   return a.first > b.first; // return smaller index first
-	else
-		a.second < b.second;
-}
-int main() {
   std::cin >> n >> m;
-  visited.reserve(n);
   inDegree.reserve(n);
-  for (int i=0; i< n;i++)
-  {
-	  pair<int, int> t;
-	  t.first = i;
-	  t.second =0;
-	  inDegree.push_back(t);
-  }
-  visited.assign(n, 0);
+  inDegree.assign(n, 0);
   vector<vector<int> > adj(n, vector<int>());
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
 	#ifdef ONE_BASE
 		adj[x-1].push_back(y-1);
-		inDegree[y-1].second++;
+		inDegree[y-1]++;
 	#else
 		adj[x].push_back(y);
-		inDegree[y].second++;
+		inDegree[y]++;
 	#endif
   }
-  for (int i =0; i< n; i++) // Sort adjList of each node in decreasing order.
-	sort(adj[i].begin(), adj[i].end(), std::greater<int>());
-	  
-  // This sorting decide which node to begin with, nodes with least inDegree should start first
-  // in case of tie, the one with least index should be processed last. This is lex-smallest topological ordering
-  sort(inDegree.begin(), inDegree.end(), sortByDegree);
-  for(int i=0; i<n; i++)
-	  if(!visited[inDegree[i].first])
-			toposort(adj, inDegree[i].first);
+  // Put nodes with 0 in-degree in priority_queue i.e. nodes with higher value is on top.
+  for (int i=0; i< n; i++)
+	if(inDegree[i]==0)
+		pq.push(i);
   
-  for(int i=0; i<n; i++)
+  if(pq.size()==0)
   {
-	#ifdef ONE_BASE
-	 std::cout <<nodes.top()+1<<" ";
-	#else
-	 std::cout <<nodes.top()<<" ";
-	#endif
-	nodes.pop();
+	  cout <<"Sandro fails."<<"\n";
+      return 0;
   }
+  while(!pq.empty())
+  {
+	int t = pq.top();
+	pq.pop();
+	toposort(adj, t);
+  }	  
 }
