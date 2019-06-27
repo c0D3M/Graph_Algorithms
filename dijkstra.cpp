0e3c8556eaf2bf51d6280946a32228c07f0e7b0a
Chitrang Srivastava <chitrang.srivastava@gmail.com>
2019-06-27 18:52:30 +0530

    Add files via upload

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 #include <cmath>
 #define INDEX 1 //set this to 1 if graph input starts with 1 as first node.
-#define UNDIRECTED  // define this to process the input for undirected graph test case
+//#define UNDIRECTED  // define this to process the input for undirected graph test case
 using std::vector;
 using std::queue;
 using std::pair;
@@ -17,53 +17,57 @@ public:
     }
 };
 
-int distance(vector<vector<int> > &adj, vector<vector<double> > &cost, int s, int t) {
-  //write your code her
-  vector<double> dist(adj.size(), INFINITY);
-  priority_queue<pair<int, int>, vector<pair<int, int>>, Compare>  p;
-  p.push(std::make_pair(s, 0));
-  dist[s] = 0;
-  vector<bool> visited(adj.size(), 0);
-  while(!p.empty())
-  {
-	  int n = p.top().first;
-	  p.pop();
-	  if(visited[n])
-			continue;
-	  for(int i =0; i< adj[n].size(); i++)
-	  {
-		  if(dist[adj[n][i]] > (dist[n] + cost[n][i])) //Relax
-		  {
-		      dist[adj[n][i]] = (dist[n] + cost[n][i]);
-			  p.push(std::make_pair(adj[n][i], dist[adj[n][i]]));
-		  }  
-	  }
-	  visited[n] = true;	
-  }
-  if(dist[t]!=INFINITY)
-	  return dist[t];
-  else
-	return -1;
-}	
+int distance(vector<vector<int> > &adj, vector<vector<double> > &cost, int s,
+        int t)
+{
+    //write your code her
+    vector<double> dist(adj.size(), INFINITY);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> p;
+    p.push(std::make_pair(s, 0));
+    dist[s] = 0;
+    vector<bool> visited(adj.size(), 0);
+    while (!p.empty())
+    {
+        int n = p.top().first;
+        p.pop();
+        if (visited[n])
+            continue;
+        for (int i = 0; i < adj[n].size(); i++)
+        {
+            if (dist[adj[n][i]] > (dist[n] + cost[n][i])) //Relax
+            {
+                dist[adj[n][i]] = (dist[n] + cost[n][i]);
+                p.push(std::make_pair(adj[n][i], dist[adj[n][i]]));
+            }
+        }
+        visited[n] = true;
+    }
+    if (dist[t] != INFINITY)
+        return dist[t];
+    else
+        return -1;
+}
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<double> > cost(n, vector<double>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - INDEX].push_back(y - INDEX);
-    cost[x - INDEX].push_back(w);
+int main()
+{
+    int n, m;
+    std::cin >> n >> m;
+    vector < vector<int> > adj(n, vector<int>());
+    vector < vector<double> > cost(n, vector<double>());
+    for (int i = 0; i < m; i++)
+    {
+        int x, y, w;
+        std::cin >> x >> y >> w;
+        adj[x - INDEX].push_back(y - INDEX);
+        cost[x - INDEX].push_back(w);
 #ifdef UNDIRECTED
-	adj[y - INDEX].push_back(x - INDEX);
-    cost[y - INDEX].push_back(w);
+        adj[y - INDEX].push_back(x - INDEX);
+        cost[y - INDEX].push_back(w);
 #endif
-  }
-  int s, t;
-  std::cin >> s >> t;
-  s = s - INDEX; 
-  t = t - INDEX;
-  std::cout << distance(adj, cost, s, t);
+    }
+    int s, t;
+    std::cin >> s >> t;
+    s = s - INDEX;
+    t = t - INDEX;
+    std::cout << distance(adj, cost, s, t);
 }
